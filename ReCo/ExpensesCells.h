cd2c1e0255c494c71ce4d8cd6d6b060c02e5bffa
Abhinav Tirath <tirathabhinav@gmail.com>
2017-10-14 17:11:33 -0400

    Filled in some TableViews

---
+++ ReCo/ExpensesCells.h
@@ -0,0 +1,18 @@
+//
+//  ExpensesCells.h
+//  ReCo
+//
+//  Created by Abhinav Tirath on 10/14/17.
+//  Copyright © 2017 ARA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpensesCells : UITableViewCell{
+    IBOutlet UILabel *amount;
+    IBOutlet UILabel *address;
+    IBOutlet UILabel *date;
+
+}
+
+@end
---
+++ ReCo/ExpensesCells.m
@@ -0,0 +1,24 @@
+//
+//  ExpensesCells.m
+//  ReCo
+//
+//  Created by Abhinav Tirath on 10/14/17.
+//  Copyright © 2017 ARA. All rights reserved.
+//
+
+#import "ExpensesCells.h"
+
+@implementation ExpensesCells
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
