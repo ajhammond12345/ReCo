bedbe30959886b24ae4743c25678c2a050b676ef
Alexander Hammond <ajhammond123@gmail.com>
2017-10-14 01:49:19 -0400

    Initial Commit

---
+++ ReCo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReCo
+//
+//  Created by Alexander Hammond on 10/14/17.
+//  Copyright © 2017 ARA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReCo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ReCo
+//
+//  Created by Alexander Hammond on 10/14/17.
+//  Copyright © 2017 ARA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
